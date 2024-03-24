a1e714236e66058c18385b29edea9b81bbc98778
Giovanni El Baruqui <98186906+giovannielbaruqui@users.noreply.github.com>
2024-03-24 11:23:24 -0300

    wip: configurando banco de dados

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const db = require('./db/connection');
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
+
+app.listen(PORT, function() {
+    console.log(`O Express está rodando na porta ${PORT}`);
+});
+
+//body parser
+app.use(bodyParser.urlencoded({extented: false}));
+
+// db connection
+db
+    .authenticate()
+    .then(() => {
+        console.log("Conectou ao banco com sucesso");
+    })
+    .catch(err => {
+        console.log("Ocorreu um erro ao conectar", err);
+    })
+
+
+//rotas
+app.get('/', (req, res) => {
+    res.send("Está funcionando 3");
+});
+
+//jobs routes
+app.use('/jobs', require('./routes/jobs'));
