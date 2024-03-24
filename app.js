6bc7a6286d539c5c25f8c0dcb375ecbc95a4813d
Giovanni El Baruqui <98186906+giovannielbaruqui@users.noreply.github.com>
2024-03-24 14:01:36 -0300

    wip: configurando handlebars

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const exphbs = require('express-handlebars'); 
 const app = express();
+const path = require('path');
 const db = require('./db/connection');
 const bodyParser = require('body-parser');
 
@@ -11,6 +13,15 @@ app.listen(PORT, function() {
 
 //body parser
 app.use(bodyParser.urlencoded({extented: false}));
+
+//handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // db connection
 db
@@ -25,7 +36,7 @@ db
 
 //rotas
 app.get('/', (req, res) => {
-    res.send("Está funcionando 3");
+    res.render('index');
 });
 
 //jobs routes
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const Job = require('../models/Job');
 
+//rota teste
 router.get('/test', (req, res) => {
     res.send('deu certo!');
+})
+
+router.get('/add', (req, res) => {
+    res.render('add');
 })
 
 // adicionando job via post
